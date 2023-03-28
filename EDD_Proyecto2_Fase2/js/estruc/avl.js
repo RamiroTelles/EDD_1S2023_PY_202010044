76c6b6e6abc163b8455e9b0af4f12fb1d73426bb
unknown <yenyogamer290@gmail.com>
2023-03-27 19:38:40 -0600

    parte arbol avl

---
+++ EDD_Proyecto2_Fase2/js/estruc/avl.js
@@ -0,0 +1,17 @@
+class nodoAVL{
+    constructor(dato){
+        this.dato = dato
+        this.altura =0
+        this.izq = null
+        this.derecha = null
+    }
+}
+
+class arbolAVL{
+    constructor(){
+        this.raiz=null
+    }
+
+    
+}
+
